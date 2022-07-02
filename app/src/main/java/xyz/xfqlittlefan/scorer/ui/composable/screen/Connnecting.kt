2a7512b9254262dda28125687020e47038d8233b
xfqwdsj <xfqwdsj@qq.com>
2022-07-02 19:10:34 +0800

    test: 测试一项新功能

--- app/src/main/java/xyz/xfqlittlefan/scorer/ui/composable/screen/Connnecting.kt
+++ app/src/main/java/xyz/xfqlittlefan/scorer/ui/composable/screen/Connnecting.kt
@@ -1,5 +1,3 @@
-@file:OptIn(ExperimentalAnimationApi::class)
-
 package xyz.xfqlittlefan.scorer.ui.composable.screen
 
 import androidx.compose.animation.AnimatedContent
@@ -10,13 +8,11 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Cancel
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.*
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -32,6 +28,7 @@ import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import xyz.xfqlittlefan.scorer.R
 import xyz.xfqlittlefan.scorer.communication.CLIENT_VERSION
@@ -58,11 +55,16 @@ fun Connecting(
         actions = {
             IconButton(
                 onClick = { viewModel.onCreatingServerButtonClick(mainViewModel) },
-                enabled = mainViewModel.server == null
+                enabled = mainViewModel.server == null && !viewModel.showSeats
             ) {
                 Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = stringResource(R.string.page_content_connecting_button_create)
+                    imageVector = Icons.Default.Add, contentDescription = stringResource(
+                        when {
+                            mainViewModel.server != null -> R.string.page_content_connecting_button_create_disabled_created
+                            viewModel.seats != null -> R.string.page_content_connecting_button_create_disabled_connected
+                            else -> R.string.page_content_connecting_button_create
+                        }
+                    )
                 )
             }
         }) {
@@ -71,7 +73,7 @@ fun Connecting(
                 .fillMaxSize()
                 .windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Horizontal + WindowInsetsSides.Bottom))
                 .verticalScroll(rememberScrollState())
-                .padding(10.dp),
+                .padding(20.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -84,18 +86,18 @@ fun Connecting(
             Spacer(Modifier.height(20.dp))
             TextField(value = viewModel.host,
                 onValueChange = viewModel::onHostChange,
-                enabled = viewModel.seats == null,
+                enabled = !viewModel.showSeats,
                 label = {
                     Text(stringResource(R.string.page_content_connecting_text_field_host_label))
                 })
             Spacer(Modifier.height(20.dp))
             TextField(value = viewModel.port,
                 onValueChange = viewModel::onPortChange,
-                enabled = viewModel.seats == null,
+                enabled = !viewModel.showSeats,
                 label = {
                     Text(stringResource(R.string.page_content_connecting_text_field_port_label))
                 })
-            AnimatedVisibility(visible = viewModel.seats != null) {
+            AnimatedVisibility(visible = viewModel.showSeats) {
                 Column {
                     Spacer(Modifier.height(20.dp))
                     FlowRow(
@@ -107,17 +109,45 @@ fun Connecting(
                                 onClick = { viewModel.selectedSeat = seat },
                                 label = {
                                     Text(stringResource(res))
-                                })
+                                }, enabled = viewModel.showSeats
+                            )
+                        }
+                    }
+
+                    DisposableEffect(Unit) {
+                        onDispose {
+                            viewModel.clearSeats()
                         }
                     }
                 }
             }
-            Spacer(Modifier.height(20.dp))
-            Button(onClick = viewModel::onGettingSeatsButtonClick) {
-                Icon(
-                    imageVector = Icons.Default.Done,
-                    contentDescription = stringResource(R.string.page_content_connecting_button_get_seats)
-                )
+            AnimatedVisibility(visible = !viewModel.showSeats) {
+                Column {
+                    Spacer(Modifier.height(20.dp))
+                    Button(
+                        onClick = viewModel::onGettingSeatsButtonClick,
+                        enabled = !viewModel.showSeats
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Done,
+                            contentDescription = stringResource(R.string.page_content_connecting_button_get_seats)
+                        )
+                    }
+                }
+            }
+            AnimatedVisibility(visible = viewModel.gettingSeatsJob != null || viewModel.showSeats) {
+                Column {
+                    Spacer(Modifier.height(20.dp))
+                    Button(
+                        onClick = viewModel::onCancelingConnectionButtonClick,
+                        enabled = viewModel.gettingSeatsJob != null || viewModel.showSeats
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Cancel,
+                            contentDescription = stringResource(R.string.page_content_connecting_button_cancel_connection)
+                        )
+                    }
+                }
             }
         }
     }
@@ -134,11 +164,14 @@ class ConnectingScreenViewModel : ViewModel() {
         port = newValue
     }
 
+    var showSeats by mutableStateOf(false)
     var seats by mutableStateOf<Map<Int, Int>?>(null)
     var selectedSeat by mutableStateOf<Int?>(null)
 
+    var gettingSeatsJob: Job? = null
+
     fun onGettingSeatsButtonClick() {
-        viewModelScope.launch(Dispatchers.IO) {
+        gettingSeatsJob = viewModelScope.launch(Dispatchers.IO) {
             val info = client.get {
                 url {
                     protocol = URLProtocol.HTTP
@@ -147,8 +180,22 @@ class ConnectingScreenViewModel : ViewModel() {
                     appendPathSegments("join", CLIENT_VERSION.toString())
                 }
             }.body<String>().decodeFromJson<WebSocketServerInfo>()
+            showSeats = true
             seats = info.seats
         }
+        gettingSeatsJob?.invokeOnCompletion {
+            gettingSeatsJob = null
+        }
+    }
+
+    fun onCancelingConnectionButtonClick() {
+        showSeats = false
+        gettingSeatsJob?.cancel()
+    }
+
+    fun clearSeats() {
+        seats = null
+        selectedSeat = null
     }
 
     fun onConnectingButtonClick(seat: Int, navController: NavController, viewModel: MainViewModel) {
@@ -162,6 +209,7 @@ class ConnectingScreenViewModel : ViewModel() {
             viewModel.server = launcher
             host = InetAddress.getLocalHost().hostAddress!!
             port = launcher.server.resolvedConnectors().first().port.toString()
+            onGettingSeatsButtonClick()
         }
     }
 
