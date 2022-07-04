c88769efad39e157b19c2f344f4f2943ae65e8d1
xfqwdsj <xfqwdsj@qq.com>
2022-07-04 23:46:35 +0800

    refactor: 删除无用文件

--- app/src/main/java/xyz/xfqlittlefan/scorer/util/ContextUtil.kt
+++
@@ -1,6 +0,0 @@
-package xyz.xfqlittlefan.scorer.util
-
-import android.content.Context
-import androidx.core.content.ContextCompat
-
-fun Context.compatCheckSelfPermission(permission: String) = ContextCompat.checkSelfPermission(this, permission)
