2c959c819993ded52921371b14c3a1059f78d547
xfqwdsj <xfqwdsj@qq.com>
2022-07-04 23:48:14 +0800

    refactor: 移除无用代码

--- app/src/main/java/xyz/xfqlittlefan/scorer/util/QRUtil.kt
+++ app/src/main/java/xyz/xfqlittlefan/scorer/util/QRUtil.kt
@@ -2,11 +2,8 @@ package xyz.xfqlittlefan.scorer.util
 
 import android.graphics.Bitmap
 import android.graphics.Color
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
-import com.google.zxing.qrcode.QRCodeReader
 import com.google.zxing.qrcode.QRCodeWriter
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 
@@ -30,12 +27,4 @@ fun generateQR(text: String): Bitmap {
     val bitmap = Bitmap.createBitmap(512, 512, Bitmap.Config.ARGB_8888)
     bitmap.setPixels(pixels, 0, 512, 0, 0, 512, 512)
     return bitmap
-}
-
-class QRAnalyzer(private val onResult: (qr: com.google.zxing.Result) -> Unit): ImageAnalysis.Analyzer {
-    private val reader = QRCodeReader()
-
-    override fun analyze(image: ImageProxy) {
-
-    }
 }
