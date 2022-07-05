f77450d72691eb598a9da7005c7c86aaacb27327
xfqwdsj <xfqwdsj@qq.com>
2022-07-05 11:53:09 +0800

    test: 测试扫描页

--- app/src/main/java/xyz/xfqlittlefan/scorer/ui/composable/screen/Scanning.kt
+++ app/src/main/java/xyz/xfqlittlefan/scorer/ui/composable/screen/Scanning.kt
@@ -1,11 +1,18 @@
 package xyz.xfqlittlefan.scorer.ui.composable.screen
 
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import xyz.xfqlittlefan.scorer.qr.QRAnalyzer
+import xyz.xfqlittlefan.scorer.ui.composable.CameraX
 
 @Composable
 fun Scanning(
@@ -13,7 +20,12 @@ fun Scanning(
     windowSize: WindowWidthSizeClass,
     viewModel: ScanningScreenViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
-
+    val context = LocalContext.current
+    CameraX(modifier = Modifier.fillMaxSize(), cameraSelectorBuilder = {
+        requireLensFacing(CameraSelector.LENS_FACING_BACK)
+    }, imageAnalyzer = QRAnalyzer {
+        Toast.makeText(context, it.text, Toast.LENGTH_LONG).show()
+    })
 }
 
 class ScanningScreenViewModel : ViewModel() {
