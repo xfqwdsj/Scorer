db55680a2ccf0378263f5c89e669aefd070e77f8
xfqwdsj <xfqwdsj@qq.com>
2022-07-06 16:52:17 +0800

    fix: 修复暗黑模式二维码颜色不会正确显示的问题

--- app/src/main/java/xyz/xfqlittlefan/scorer/util/ColorUtil.kt
+++ app/src/main/java/xyz/xfqlittlefan/scorer/util/ColorUtil.kt
@@ -5,9 +5,9 @@ import androidx.compose.ui.graphics.ColorMatrix
 
 fun filteredWhiteColorMatrixWithTint(tint: Color) = ColorMatrix(
     floatArrayOf(
-        0f, 0f, 0f, 0f, tint.red,
-        0f, 0f, 0f, 0f, tint.blue,
-        0f, 0f, 0f, 0f, tint.blue,
+        0f, 0f, 0f, 0f, tint.red * 255,
+        0f, 0f, 0f, 0f, tint.green * 255,
+        0f, 0f, 0f, 0f, tint.blue * 255,
         -1f, -1f, -1f, 0f, 255f * 3
     )
 )
